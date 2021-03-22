e41d78ca06d225986babf393959a51b440f146b7
Jason Line <jasonline87@gmail.com>
2021-03-22 14:48:53 -0400

    test

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
+const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    ​​​​​​​​
     dialect: 'postgres',
-    dialectOptions: {
-        ​​​​​​​​
-        ssl: {
-            ​​​​​​​​
-            require: true,
-            rejectUnauthorized: false, // very important
-        }​​​​​​​​
-    }​​​​​​​​
-}​​​​​​​​)
+})
+sequelize.authenticate().then(
+    function () {
+        console.log("Connected to movie postgres database");
+    },
+    function (err) {
+        console.log(err);
+    }
+);
+
+module.exports = sequelize
