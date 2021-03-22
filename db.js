e213f4bdcd26c0ba5bdce45fa573ad8668960771
Jason Line <jasonline87@gmail.com>
2021-03-22 14:20:09 -0400

    message

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
-const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+})
 
-const db = new Sequelize("postgresql://postgres:password@localhost/newMovieDb");
+// const db = new Sequelize("postgresql://postgres:password@localhost/newMovieDb");
 
 module.exports = db;
